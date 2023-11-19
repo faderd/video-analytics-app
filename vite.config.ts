1d3c0ccb825399d6fa2e2f95d0664b3255ecb94b
faderd <andrey167000@gmail.com>
2023-11-19 15:10:52 +0300

    Добавляет сценарий тестирования vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types='vitest' />
+/// <reference types='vite/client' />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,4 +8,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '',
   plugins: [react()],
+  test: {
+    globals: true,
+  },
 });
